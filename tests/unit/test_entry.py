1b28a1903c764cd867e54a7600405f8137ff23ce
Kevin L. Mitchell <kevin.mitchell@rackspace.com>
2015-05-13 13:56:04 -0500

    Entrypoint handling
    I opted to do this without stevedore as stevedore doesn't really
    match the needs timid has for endpoint handling (even though it
    *could* technically be used) and the custom code isn't all that
    hard.

---
+++ tests/unit/test_entry.py
@@ -0,0 +1,184 @@
+# Copyright 2015 Rackspace
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the
+#    License. You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing,
+#    software distributed under the License is distributed on an "AS
+#    IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+#    express or implied. See the License for the specific language
+#    governing permissions and limitations under the License.
+
+import unittest
+
+import mock
+import pkg_resources
+import six
+
+from timid import entry
+
+
+class NamespaceCacheTest(unittest.TestCase):
+    @mock.patch.object(pkg_resources, 'iter_entry_points')
+    def test_init(self, mock_iter_entry_points):
+        eps = [
+            mock.Mock(ep_name='ep1', inst='ep1.1'),
+            mock.Mock(ep_name='ep2', inst='ep2.1'),
+            mock.Mock(ep_name='ep1', inst='ep1.2'),
+        ]
+        for ep in eps:
+            ep.name = ep.ep_name
+        mock_iter_entry_points.return_value = eps
+
+        result = entry.NamespaceCache('namespace')
+
+        self.assertEqual(result.namespace, 'namespace')
+        self.assertEqual(result._entrypoints, {
+            'ep1': [eps[0], eps[2]],
+            'ep2': [eps[1]],
+        })
+        self.assertEqual(result._epcache, {})
+
+    def make_obj(self, namespace='namespace', entrypoints=None, epcache=None):
+        with mock.patch.object(entry.NamespaceCache, '__init__',
+                               return_value=None):
+            obj = entry.NamespaceCache()
+
+        obj.namespace = namespace
+        obj._entrypoints = entrypoints or {}
+        obj._epcache = epcache or {}
+
+        return obj
+
+    def test_contains_empty(self):
+        obj = self.make_obj()
+
+        self.assertFalse('spam' in obj)
+
+    def test_contains_no_entrypoint(self):
+        obj = self.make_obj(epcache={'spam': 'ep'})
+
+        self.assertFalse('spam' in obj)
+
+    def test_contains_ep_unavailable(self):
+        obj = self.make_obj(entrypoints={'spam': []},
+                            epcache={'spam': entry._unavailable})
+
+        self.assertFalse('spam' in obj)
+
+    def test_contains_with_entrypoint(self):
+        obj = self.make_obj(entrypoints={'spam': []})
+
+        self.assertTrue('spam' in obj)
+
+    def test_getitem_empty(self):
+        obj = self.make_obj()
+
+        self.assertRaises(KeyError, lambda: obj['spam'])
+        self.assertFalse('spam' in obj._epcache)
+
+    def test_getitem_no_entrypoint(self):
+        obj = self.make_obj(epcache={'spam': 'cached'})
+
+        self.assertRaises(KeyError, lambda: obj['spam'])
+        self.assertEqual(obj._epcache, {'spam': 'cached'})
+
+    def test_getitem_ep_unavailable(self):
+        obj = self.make_obj(entrypoints={'spam': []},
+                            epcache={'spam': entry._unavailable})
+
+        self.assertRaises(KeyError, lambda: obj['spam'])
+        self.assertEqual(obj._epcache, {'spam': entry._unavailable})
+
+    def test_getitem_ep_cached(self):
+        obj = self.make_obj(entrypoints={'spam': []},
+                            epcache={'spam': 'cached'})
+
+        self.assertEqual(obj['spam'], 'cached')
+        self.assertEqual(obj._epcache, {'spam': 'cached'})
+
+    def test_getitem_ep_emptylist(self):
+        obj = self.make_obj(entrypoints={'spam': []})
+
+        self.assertRaises(KeyError, lambda: obj['spam'])
+        self.assertEqual(obj._epcache, {'spam': entry._unavailable})
+
+    def test_getitem_ep_errors(self):
+        eps = [
+            mock.Mock(**{'load.side_effect': ImportError('spam')}),
+            mock.Mock(**{'load.side_effect': AttributeError('spam')}),
+            mock.Mock(**{
+                'load.side_effect': pkg_resources.UnknownExtra('spam'),
+            }),
+        ]
+        obj = self.make_obj(entrypoints={'spam': eps})
+
+        self.assertRaises(ImportError, lambda: obj['spam'])
+        self.assertEqual(obj._epcache, {'spam': entry._unavailable})
+        for ep in eps:
+            ep.load.assert_called_once_with()
+
+    def test_getitem_ep_success(self):
+        eps = [
+            mock.Mock(**{
+                'load.side_effect': ImportError('spam'),
+                'load_expected': True,
+            }),
+            mock.Mock(**{
+                'load.side_effect': AttributeError('spam'),
+                'load_expected': True,
+            }),
+            mock.Mock(**{
+                'load.side_effect': pkg_resources.UnknownExtra('spam'),
+                'load_expected': True,
+            }),
+            mock.Mock(**{
+                'load.return_value': 'object',
+                'load_expected': True,
+            }),
+            mock.Mock(**{
+                'load.return_value': 'other',
+                'load_expected': False,
+            }),
+        ]
+        obj = self.make_obj(entrypoints={'spam': eps})
+
+        self.assertEqual(obj['spam'], 'object')
+        self.assertEqual(obj._epcache, {'spam': 'object'})
+        for ep in eps:
+            if ep.load_expected:
+                ep.load.assert_called_once_with()
+            else:
+                self.assertFalse(ep.load.called)
+
+
+class EntrypointCacheTest(unittest.TestCase):
+    def test_init(self):
+        result = entry.EntrypointCache()
+
+        self.assertEqual(result._namespaces, {})
+
+    @mock.patch.object(entry, 'NamespaceCache', return_value='ns_cache')
+    def test_getitem_cached(self, mock_NamespaceCache):
+        obj = entry.EntrypointCache()
+        obj._namespaces['spam'] = 'cached'
+
+        self.assertEqual(obj['spam'], 'cached')
+        self.assertEqual(obj._namespaces, {'spam': 'cached'})
+        self.assertFalse(mock_NamespaceCache.called)
+
+    @mock.patch.object(entry, 'NamespaceCache', return_value='ns_cache')
+    def test_getitem_uncached(self, mock_NamespaceCache):
+        obj = entry.EntrypointCache()
+
+        self.assertEqual(obj['spam'], 'ns_cache')
+        self.assertEqual(obj._namespaces, {'spam': 'ns_cache'})
+        mock_NamespaceCache.assert_called_once_with('spam')
+
+    def test_points(self):
+        self.assertTrue(isinstance(entry.points, entry.EntrypointCache))
+        self.assertEqual(entry.points._namespaces, {})
---
+++ timid/entry.py
@@ -0,0 +1,136 @@
+# Copyright 2015 Rackspace
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the
+#    License. You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing,
+#    software distributed under the License is distributed on an "AS
+#    IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+#    express or implied. See the License for the specific language
+#    governing permissions and limitations under the License.
+
+import sys
+
+import pkg_resources
+import six
+
+
+# Indicate that the entrypoint cannot be loaded
+_unavailable = object()
+
+
+class NamespaceCache(object):
+    """
+    Cache of loaded entrypoints for a designated namespace.  A given
+    entrypoint may be accessed using item notation.
+    """
+
+    def __init__(self, namespace):
+        """
+        Initialize a ``NamespaceCache``.
+
+        :param namespace: The namespace to cache.
+        """
+
+        # Save the namespace
+        self.namespace = namespace
+
+        # Formulate the entrypoints so we only iterate once
+        self._entrypoints = {}
+        for ep in pkg_resources.iter_entry_points(namespace):
+            self._entrypoints.setdefault(ep.name, [])
+            self._entrypoints[ep.name].append(ep)
+
+        # Cache of successfully loaded entrypoints
+        self._epcache = {}
+
+    def __contains__(self, name):
+        """
+        Determine if an entrypoint is available.  Note that no attempt is
+        made to load the entrypoint, so referencing the entrypoint
+        later may raise an exception.
+
+        :param name: The name of the entrypoint.
+
+        :returns: A ``True`` or ``False`` value, depending on if the
+                  entrypoint is available.
+        """
+
+        return (name in self._entrypoints and
+                self._epcache.get(name) is not _unavailable)
+
+    def __getitem__(self, name):
+        """
+        Retrieve the designated entrypoint object.
+
+        :param name: The name of the entrypoint.
+
+        :returns: The loaded object referenced by the entrypoint.
+        """
+
+        # If it's not defined, or if we failed to load it last time,
+        # bail out with a KeyError
+        if (name not in self._entrypoints or
+                self._epcache.get(name) is _unavailable):
+            raise KeyError(name)
+
+        # OK, do we need to load it?
+        if name not in self._epcache:
+            error = None
+            for ep in self._entrypoints[name]:
+                try:
+                    self._epcache[name] = ep.load()
+                except (ImportError, AttributeError,
+                        pkg_resources.UnknownExtra):
+                    # Save the error for later re-raise
+                    if error is None:
+                        error = sys.exc_info()
+                else:
+                    # Successfully resolved it
+                    break
+            else:
+                # Couldn't resolve it...
+                self._epcache[name] = _unavailable
+                if error is None:
+                    raise KeyError(name)
+                six.reraise(*error)
+
+        return self._epcache[name]
+
+
+class EntrypointCache(object):
+    """
+    Cache of entrypoint namespaces.  A given namespace may be accessed
+    using item notation.
+    """
+
+    def __init__(self):
+        """
+        Initialize an ``EntrypointCache``.
+        """
+
+        self._namespaces = {}
+
+    def __getitem__(self, name):
+        """
+        Retrieve a ``NamespaceCache`` for a designated entrypoint
+        namespace.
+
+        :param name: The entrypoint namespace.
+
+        :returns: An instance of ``NamespaceCache``.
+        """
+
+        # Look up the namespace
+        if name not in self._namespaces:
+            self._namespaces[name] = NamespaceCache(name)
+
+        return self._namespaces[name]
+
+
+# The cache
+points = EntrypointCache()
